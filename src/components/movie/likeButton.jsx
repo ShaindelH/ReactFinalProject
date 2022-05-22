a4f633c0f265b7db71321f2d53437e43c8224fa9
ShaindelH <91918306+ShaindelH@users.noreply.github.com>
2022-05-22 00:41:20 -0400

    Final Version

--- src/components/movie/likeButton.jsx
+++ src/components/movie/likeButton.jsx
@@ -1,41 +1,38 @@
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import React, { useContext } from "react";
-import {MovieContext} from "../state/movieContext";
-import {IconButton, Tooltip} from "@mui/material";
+import { MovieContext } from "../state/movieContext";
+import { IconButton, Tooltip } from "@mui/material";
 
-export const LikeButton = ({movie}) => {
-  
-  const {likedMovies, setLikedMovies} = useContext(MovieContext);  
+export const LikeButton = ({ movie }) => {
+  const { likedMovies, setLikedMovies } = useContext(MovieContext);
 
   function handleLikeClick() {
-   
-    if(likedMovies.has(Number(movie.id))){
-    }
-    else{
-      likedMovies.set(Number(movie.id), {movie});
+    if (likedMovies.has(Number(movie.id))) {
+      likedMovies.delete(Number(movie.id));
+    } else {
+      likedMovies.set(Number(movie.id), { movie });
     }
     let copy = new Map(likedMovies);
     setLikedMovies(copy);
   }
-   
+
   return (
-   
     <Tooltip title="Like Movie">
-    <IconButton
-      size="medium"
-      sx={{ marginLeft: 2, "&:hover": { color: "green" } }}
-      onClick={handleLikeClick}
-    >
-      {likedMovies.has(movie.id) ? (
-        <FavoriteIcon sx={{ color: "red" }} />
-      ) : (
-        <FavoriteBorderOutlinedIcon
-          sx={{ color: "red" }}
-          className="likeButton"
-        />
-      )}
-    </IconButton>
+      <IconButton
+        size="medium"
+        sx={{ marginLeft: 2, "&:hover": { color: "green" } }}
+        onClick={handleLikeClick}
+      >
+        {likedMovies.has(movie.id) ? (
+          <FavoriteIcon sx={{ color: "red" }} />
+        ) : (
+          <FavoriteBorderOutlinedIcon
+            sx={{ color: "red" }}
+            className="likeButton"
+          />
+        )}
+      </IconButton>
     </Tooltip>
   );
 };
